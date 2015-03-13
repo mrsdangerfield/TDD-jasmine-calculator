80e641f9c9a43478766e19791c2865ec1070736c
Jade Brookes <jade.brookes@tribalworldwide.com>
2015-03-13 17:05:54 +0000

    Initial commit to the base

---
+++ jasmine/spec/calculator_spec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function(){
+	it("should add numbers", function() {
+		expect(Calculator.add(5)).toEqual(5);
+		expect(Calculator.add(5)).toEqual(10);
+	});
+	
+});
---
+++ jasmine/src/calculator.js
@@ -0,0 +1,5 @@
+window.Calculator = {
+	add: function( num1) {
+		return 0 + num1;
+	}
+};
